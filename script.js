f0a40abb2b2e2759b46af0b13b1bb39ff6bfaa1d
Naitik Sahu <naiitik1526@gmail.com>
2026-09-23 13:36:34 +0530

    edited css and added started working on terminal interface

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ function showLine(){
     if(num<lines.length){
         lines[num].style.visibility="visible";
         num++;
-        setTimeout(showLine, 300)
+        if(num==lines.length){
+            setTimeout(showCorruption, 2000)
+        }
+        else{
+            setTimeout(showLine, 500)
+        }
     }
+    
 }
-showLine()
+
+function showCorruption(){
+    let area= document.getElementById("corruption");
+    area.innerHTML="<p>CLOCK SOURCE: UNKNOWN</p>";
+    setTimeout(function(){
+        area.innerHTML+="<p>ATTEMPTING CLOCK RECOVERY...</P>";
+    },1000);
+}
+
+showLine();
+
+const terminal=document.getElementById("terminal-interface");
+const input=document.getElementById("command-input");
+const output =document.getElementById("terminal-output");
